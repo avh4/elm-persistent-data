b67fa49615a22d83116f00625205ec4a7589a6f6
Aaron VonderHaar <gruen0aermel@gmail.com>
2017-01-19 23:09:04 -0800

    Enable CORS for example-server

--- example-server/server.js
+++ example-server/server.js
@@ -10,7 +10,8 @@ const PORT = 8080
 
 app.use(function (req, res, next) {
   res.header('Access-Control-Allow-Origin', '*')
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+  res.header('Access-Control-Allow-Methods', 'GET, PUT')
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, x-if-match, x-if-empty')
   next()
 })
 
