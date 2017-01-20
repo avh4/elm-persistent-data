fe199b02ef929aae3ba63bf4117ace8d582cc44b
Aaron VonderHaar <gruen0aermel@gmail.com>
2017-01-19 23:19:54 -0800

    Extract validateKeys middleware in example-server

--- example-server/server.js
+++ example-server/server.js
@@ -8,11 +8,21 @@ app.use(morgan('tiny'))
 
 const PORT = process.env.PORT || 8080
 
-app.use(function (req, res, next) {
+app.use(function enableCors (req, res, next) {
   res.header('Access-Control-Allow-Origin', '*')
   res.header('Access-Control-Allow-Methods', 'GET, PUT')
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, x-if-match, x-if-empty')
   next()
+})
+
+app.use(function validateKeys (req, res, next) {
+  if (req.url.match(/^\/refs\//) && !req.url.match(/^\/refs\/([-.a-zA-Z0-9]*)$/)) {
+    res.sendStatus(404)
+  } else if (req.url.match(/^\/content\//) && !req.url.match(/^\/content\/(sha256-[a-f0-9]{64})$/)) {
+    res.sendStatus(404)
+  } else {
+    next()
+  }
 })
 
 function handleWrite (key, req, res) {
@@ -34,55 +44,39 @@ app.get('/', function (req, res) {
 })
 
 app.get('/refs/:key', function (req, res) {
-  if (req.url.match(/^\/refs\/([-.a-zA-Z0-9]*)$/)) {
-    res.sendFile(req.params.key, { root: path.join(__dirname, '/refs') })
-  } else {
-    res.sendStatus(404)
-  }
+  res.sendFile(req.params.key, { root: path.join(__dirname, '/refs') })
 })
 
 app.put('/refs/:key', function (req, res) {
-  if (req.url.match(/^\/refs\/([-.a-zA-Z0-9]*)$/)) {
-    var key = req.params.key
-    if (req.headers['x-if-empty']) {
-      var exists = fs.existsSync('refs/' + key)
-      if (exists) {
-        res.status(400).send('x-if-empty was set, but ref currently exists')
-        // console.log('400 PUT refs/' + key + ' (already exists)')
-      } else {
-        handleWrite('refs/' + key, req, res)
-      }
-    } else if (req.headers['x-if-match']) {
-      var actual = fs.readFileSync('refs/' + key, 'utf8')
-      var expected = req.headers['x-if-match']
-      if (actual !== expected) {
-        res.status(400).send('x-if-match was set, but ref does not match')
-        // console.log('400 PUT refs/' + key + ' (' + actual + ' !== ' + expected + ')')
-      } else {
-        handleWrite('refs/' + key, req, res)
-      }
+  var key = req.params.key
+  if (req.headers['x-if-empty']) {
+    var exists = fs.existsSync('refs/' + key)
+    if (exists) {
+      res.status(400).send('x-if-empty was set, but ref currently exists')
+      // console.log('400 PUT refs/' + key + ' (already exists)')
     } else {
-      res.status(400).send('x-if-empty or x-if-match header is required')
+      handleWrite('refs/' + key, req, res)
+    }
+  } else if (req.headers['x-if-match']) {
+    var actual = fs.readFileSync('refs/' + key, 'utf8')
+    var expected = req.headers['x-if-match']
+    if (actual !== expected) {
+      res.status(400).send('x-if-match was set, but ref does not match')
+      // console.log('400 PUT refs/' + key + ' (' + actual + ' !== ' + expected + ')')
+    } else {
+      handleWrite('refs/' + key, req, res)
     }
   } else {
-    res.sendStatus(404)
+    res.status(400).send('x-if-empty or x-if-match header is required')
   }
 })
 
 app.get('/content/:key', function (req, res) {
-  if (req.url.match(/^\/content\/(sha256-[a-f0-9]{64})$/)) {
-    res.sendFile(req.params.key, { root: path.join(__dirname, '/content') })
-  } else {
-    res.sendStatus(404)
-  }
+  res.sendFile(req.params.key, { root: path.join(__dirname, '/content') })
 })
 
 app.put('/content/:key', function (req, res) {
-  if (req.url.match(/^\/content\/(sha256-[a-f0-9]{64})$/)) {
-    handleWrite('content/' + req.params.key, req, res)
-  } else {
-    res.sendStatus(404)
-  }
+  handleWrite('content/' + req.params.key, req, res)
 })
 
 app.listen(PORT, function () {
