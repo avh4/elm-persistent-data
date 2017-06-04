85fbb73fecb13d93386eec94876ee7bd521ad399
Aaron VonderHaar <gruen0aermel@gmail.com>
2017-06-04 14:04:26 -0700

    Initializer: clean up

--- src/Persistence/Initializer.elm
+++ src/Persistence/Initializer.elm
@@ -1,27 +1,38 @@
-module Persistence.Initializer exposing (Next(..), init)
+module Persistence.Initializer exposing (Config, Next(..), init)
 
 
-type Next id data msg
-    = Done (Maybe id) data
-    | FetchBatch id (Result ( List msg, Maybe id ) data -> Next id data msg)
-
-
-init :
+type alias Config id data msg =
     { cachedData : Maybe { root : id, data : data }
     , latestRoot : Maybe id
     , emptyData : data
     , fold : msg -> data -> data
     }
-    -> Next id data msg
+
+
+{-| The result of initializing an event store.
+
+  - Done: initialization is complete, and the resulting root id and data are provided
+  - FetchBatch: to continue initialization, you must fetch that batch of the given id and call the given function.
+    If you have computed data cached for that id, you may pass `Ok data`,
+    otherwise fetch the batch and pass `Err (batchMsgs, batchParentId)`.
+
+-}
+type Next id data msg
+    = Done (Maybe id) data
+    | FetchBatch id (Result ( List msg, Maybe id ) data -> Next id data msg)
+
+
+init : Config id data msg -> Next id data msg
 init config =
     handleParent config config.latestRoot []
 
 
+handleParent : Config id data msg -> Maybe id -> List (List msg) -> Next id data msg
 handleParent config next allMsgs =
     let
         done d =
             Done config.latestRoot
-                -- TODO: optimize by not using List.concat
+                -- TODO: optimize by using a nested fold instead of List.concat
                 (List.foldl config.fold d (List.concat allMsgs))
     in
     case next of
